8736115c83c01eb82f9393b727e4883a4aafa0c5
Reisha Puranik <reishapuranik@148-100-145-35.FoxNet.marist.edu>
2019-09-17 17:31:48 -0400

    Solutions to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,9 @@
+# Introduction to Programming
+# Author: Reisha Puranik
+# Date: September 17, 2019
+def main():
+    s = 186000
+    d = 34000000
+    t = (d/s)
+    print("The time it takes to a photo from Curiosity to reach Nasa when Mars is at its closest orbit to Earth is: ", t)
+main()
