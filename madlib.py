ba86b37ed695e38dbc7068a239d70b51ec9d274e
Reisha Puranik <reishapuranik@148-100-145-35.FoxNet.marist.edu>
2019-09-17 17:57:47 -0400

    Solution to Mad Libs Exercise.

---
+++ madlib.py
@@ -0,0 +1,11 @@
+# Introduction to Programming
+# Author: Reisha Puranik
+# Date: September 17, 2019
+def main():
+    n = input("Enter a noun:")
+    v = input("Enter a verb:")
+    adj = input("Enter an adjective:")
+    place = input("Enter a place:")
+    print("Take your", adj, n, "and", v, "it at the", place!)
+main()
+    
