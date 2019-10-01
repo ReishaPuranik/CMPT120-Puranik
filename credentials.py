1c2750e7f538038b0f57857d22bd68e32ead7ab5
Reisha Puranik <reishapuranik@148-100-145-35.FoxNet.marist.edu>
2019-10-01 17:43:27 -0400

    EDIT on the first section

--- credentials.py
+++ credentials.py
@@ -6,17 +6,15 @@ def main():
     # get user's first and last names
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
-    uname = first + last
+    uname = first + "." + last
     print(uname)
 # TODO modify this to generate a Marist-style username uname = first + last
 # ask user to create a new password
     passwd = input("Create a new password with at least 8 characters: ")    
 # TODO modify this to ensure the password has at least 8 characters
-    while len(passwd) >= 8:
-        print("The force is strong in this one...")
-        print("Account configured. Your new email address is", uname,"@marist.edu")
-        break
-    else:
+    while len(passwd) < 8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
+    print("The force is strong in this one...")
+    print("Account configured. Your new email address is", uname,"@marist.edu")
 main()
