dbdfe4eeaef58439fa598d8966bc6f3084b1c3c8
Reisha Puranik <reishapuranik@148-100-145-35.FoxNet.marist.edu>
2019-10-15 17:57:52 -0400

    Usability improvements to the guessing game, resolves q and yes and no issue.

--- guessing-game.py
+++ guessing-game.py
@@ -6,12 +6,17 @@ def main():
     while True:
         print("The program is thinking of an animal")
         guess = input("Guess the name of the animal: ")
-        if guess == "quit":
+        if guess[0] == "q":
             print("You quit the game.")
             break
         elif guess.lower() == name.lower():
             print("Congratulations you guessed correctly! The animal name is dog")
-            break
+            favor=input("Do you like this animal?:")
+            if favor == "y":
+                print("me too")
+            if favor == "n":
+                print("Aw ok")
+            break   
         else:
             print("Wrong guess. Please try again")
         
