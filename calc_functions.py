5938f095b9ec2f0818781a8c45564cded46462fe
Reisha Puranik <reishapuranik@148-100-145-35.FoxNet.marist.edu>
2019-10-16 20:46:58 -0400

    I made this version cleaner using comments.

--- calc_functions.py
+++ calc_functions.py
@@ -2,9 +2,11 @@
 # Author: Reisha Puranik
 # Date: October 11, 2019
 
-def calculator():
+
+def main():
     mylist = input("Enter equation to be solved: ").split()
     while len(mylist) > 1:
+        #This operation divides numbers
         if "/" in mylist:
             index = mylist.index('/')
             num1 = float(mylist[index - 1])
@@ -12,6 +14,7 @@ def calculator():
             del(mylist[index-1:index+2])
             mylist.insert(index - 1, num1/num2)
             print(mylist)
+        #This operation multiplies numbers
         elif "*" in mylist:
             index = mylist.index('*')
             num1 = float(mylist[index - 1])
@@ -19,6 +22,7 @@ def calculator():
             del(mylist[index-1:index+2])
             mylist.insert(index - 1, num1*num2)
             print(mylist)
+        #This operation adds numbers
         elif "+" in mylist:
             index = mylist.index('+')
             num1 = float(mylist[index - 1])
@@ -26,6 +30,7 @@ def calculator():
             del(mylist[index-1:index+2])
             mylist.insert(index - 1, num1+num2)
             print(mylist)
+        #This operation subtracts numbers
         elif "-" in mylist:
             index = mylist.index('-')
             num1 = float(mylist[index - 1])
@@ -35,12 +40,7 @@ def calculator():
             print(mylist)
         else:
             print("Invalid operation")
-        
-        
-
-calculator()
+            
+main()
 
 
-
-    
-        
