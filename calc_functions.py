3b611dc5ec6b59194b5fe581d22eb8ebde48f866
Reisha Puranik <reishapuranik@148-100-145-35.FoxNet.marist.edu>
2019-10-24 18:24:21 -0400

    Fixes

--- calc_functions.py
+++ calc_functions.py
@@ -3,44 +3,50 @@
 # Date: October 11, 2019
 
 
-def main():
-    mylist = input("Enter equation to be solved: ").split()
-    while len(mylist) > 1:
-        #This operation divides numbers
-        if "/" in mylist:
-            index = mylist.index('/')
-            num1 = float(mylist[index - 1])
-            num2 = float(mylist[index + 1])
-            del(mylist[index-1:index+2])
-            mylist.insert(index - 1, num1/num2)
-            print(mylist)
-        #This operation multiplies numbers
-        elif "*" in mylist:
-            index = mylist.index('*')
-            num1 = float(mylist[index - 1])
-            num2 = float(mylist[index + 1])
-            del(mylist[index-1:index+2])
-            mylist.insert(index - 1, num1*num2)
-            print(mylist)
-        #This operation adds numbers
-        elif "+" in mylist:
-            index = mylist.index('+')
-            num1 = float(mylist[index - 1])
-            num2 = float(mylist[index + 1])
-            del(mylist[index-1:index+2])
-            mylist.insert(index - 1, num1+num2)
-            print(mylist)
-        #This operation subtracts numbers
-        elif "-" in mylist:
-            index = mylist.index('-')
-            num1 = float(mylist[index - 1])
-            num2 = float(mylist[index + 1])
-            del(mylist[index-1:index+2])
-            mylist.insert(index - 1, num1-num2)
-            print(mylist)
-        else:
-            print("Invalid operation")
-            
-main()
+def process(eq, op):
+    if op == '/':
+        print("here",eq)
+        index = eq.index('/')
+        op1 = float(eq[index - 1])
+        op2 = float(eq[index + 1])
+        result = op1 / op2
+        del(eq[index - 1:index + 2])
+        eq.insert(index-1, result)
+    elif op == '*':
+        index = eq.index('*')
+        op1 = float(eq[index - 1])
+        op2 = float(eq[index + 1])
+        result = op1 * op2
+        del(eq[index - 1:index + 2])
+        eq.insert(index-1, result)
+    elif op == '+':
+        index = eq.index('+')
+        op1 = float(eq[index - 1])
+        op2 = float(eq[index + 1])
+        result = op1 + op2
+        del(eq[index - 1:index + 2])
+        eq.insert(index - 1, result)
+    elif op == '-':
+        index = eq.index('-')
+        op1 = float(eq[index - 1])
+        op2 = float(eq[index + 1])
+        result = op1 - op2
+        del(eq[index - 1:index + 2])
+        eq.insert(index-1, result)
+    else:
+        print("Invalid operation")
 
+
+def solve(eqStr):
+    eq = eqStr.split()
+    while len(eq) > 1:
+        if '/' in eq:
+            process(eq, '/')
+        elif '*' in eq:
+            process(eq, '*')
+        elif '+' in eq:
+            process(eq, '+')
+        elif '-' in eq:
+            process(eq, '-')
+    return eq[0]
 
